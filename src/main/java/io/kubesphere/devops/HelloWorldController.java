fac774fca2932a14f2dd8227756502eea739b131
Ray Zhou <ray@yunify.com>
2020-08-19 16:25:17 +0800

    Test commit

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -14,6 +14,6 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
-        return "Really appreaciate your star, that's the power of our life.";
+        return "Hi! Really appreaciate your star, that's the power of our life.";
     }
 }
