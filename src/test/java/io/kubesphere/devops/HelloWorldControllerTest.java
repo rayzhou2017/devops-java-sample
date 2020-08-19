25313be30bd2016c4754675108fb21094bd45035
Ray Zhou <ray@yunify.com>
2020-08-19 16:30:48 +0800

    Fix test

--- src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
+++ src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
@@ -8,6 +8,6 @@ public class HelloWorldControllerTest {
 
     @Test
     public void testSayHello() {
-        assertEquals("Really appreaciate your star, that's the power of our life.", new HelloWorldController().sayHello());
+        assertEquals("Hi! Really appreaciate your star, that's the power of our life.", new HelloWorldController().sayHello());
     }
 }
